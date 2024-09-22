27c2efbbbeb7516faabadbb0a0c9f914a7795c63
darkjaffs <hurairajaffs2003@gmail.com>
2024-09-22 13:08:40 +0500

    Final Commit

--- Week-7/social-media-app/src/_auth/AuthLayout.tsx
+++ Week-7/social-media-app/src/_auth/AuthLayout.tsx
@@ -1,30 +1,27 @@
 import { Outlet, Navigate } from "react-router-dom";
 
+import { useUserContext } from "@/context/AuthContext";
 
-const AuthLayout = () => {
-  const isAuthenticated = false;  
-
+export default function AuthLayout() {
+  const { isAuthenticated } = useUserContext();
 
   return (
     <>
-      { isAuthenticated ? (
-        <Navigate to='/' />
+      {isAuthenticated ? (
+        <Navigate to="/" />
       ) : (
-        <> 
-          <section className= 'flex flex-1 justify-center items-center flex-col py-10'>
+        <>
+          <section className="flex flex-1 justify-center items-center flex-col py-10">
             <Outlet />
           </section>
 
-          <img 
+          <img
             src="/assets/images/side-img.svg"
             alt="logo"
             className="hidden xl:block h-screen w-1/2 object-cover bg-no-repeat"
           />
         </>
-      )
-    }
+      )}
     </>
-  )
+  );
 }
-
-export default AuthLayout
